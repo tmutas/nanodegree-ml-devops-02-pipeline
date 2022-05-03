5b71fb2b568435e9d4ca12460fd55d6ebe95ca41
Timo Mutas <tmutas@users.noreply.github.com>
2022-05-03 08:40:55 +0200

    Add remote data_split step

--- main.py
+++ main.py
@@ -76,7 +76,15 @@ def go(config: DictConfig):
             ##################
             # Implement here #
             ##################
-            pass
+            _ = mlflow.run(
+                f"{config['main']['components_repository']}/train_val_test_split",
+                parameters={
+                    "input" : f'{config["cleaning"]["output_artifact"]}:latest',
+                    "test_size" : config["modeling"]["test_size"],
+                    "random_seed" : config["modeling"]["random_seed"],
+                    "stratify_by" : config["modeling"]["stratify_by"],
+                },
+            )
 
         if "train_random_forest" in active_steps:
 
